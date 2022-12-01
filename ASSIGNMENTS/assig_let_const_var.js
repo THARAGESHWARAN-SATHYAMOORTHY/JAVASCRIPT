f907d9b79b473efdc1b3d5f892d8d8cb7475c8e7
THARAGESHWARAN-SATHYAMOORTHY <119523694+THARAGESHWARAN-SATHYAMOORTHY@users.noreply.github.com>
2022-12-01 20:33:50 +0530

    Delete assig_let_const_var.js

--- ASSIGNMENTS/assig_let_const_var.js
+++
@@ -1,5 +0,0 @@
-let language = 'tamil';
-const mother_country = 'INDIA';
-console.log(language);
-console.log(mother_country);
-//mother_country = 'Uk';//Reassigning of constant values is not possible.
