e160bd083797e8584c218d111ee20720e7c5ee53
THARAGESHWARAN-SATHYAMOORTHY <119523694+THARAGESHWARAN-SATHYAMOORTHY@users.noreply.github.com>
2022-12-01 20:40:54 +0530

    Delete CHALLENGES directory

--- CHALLENGES/coding_challenge4.js
+++
@@ -1,3 +0,0 @@
-const bill = 275;
-let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.20 * bill;
-console.log(`The bill was ${bill}, the tip was ${(tip).toFixed(2)} and the total value was ${(Number(bill) + Number((tip).toFixed(2)))}.`);
