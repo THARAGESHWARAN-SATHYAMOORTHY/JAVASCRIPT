bb29970b44e02e8f48b10038d623d9cca23b4c9d
THARAGESHWARAN-SATHYAMOORTHY <119523694+THARAGESHWARAN-SATHYAMOORTHY@users.noreply.github.com>
2022-12-01 20:42:27 +0530

    Add files via upload

---
+++ CHALLENGES/FUNDAMENTAL_1/coding_challenge1.js
@@ -0,0 +1,15 @@
+/*const mark_weight = 78;
+const mark_height = 1.69;
+const john_weight = 92;
+const john_height = 1.95;
+*/
+const mark_weight = 95;
+const mark_height = 1.88;
+const john_weight = 85;
+const john_height = 1.76;
+
+const BMI_mark = mark_weight / (mark_height ** 2);
+const BMI_john = john_weight / (john_height ** 2);
+
+console.log(BMI_mark, BMI_john, BMI_mark > BMI_john);
+
