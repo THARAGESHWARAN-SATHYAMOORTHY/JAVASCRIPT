3b59aa25fe7cda7311fe2f8fbdb87cf39370d1f0
THARAGESHWARAN-SATHYAMOORTHY <119523694+THARAGESHWARAN-SATHYAMOORTHY@users.noreply.github.com>
2022-12-01 20:33:05 +0530

    Delete assig_Data_Types.js

--- ASSIGNMENTS/assig_Data_Types.js
+++
@@ -1,9 +0,0 @@
-let isIsland = "indira Point";
-let country;
-let population = 1.417;
-let language = true;
-
-console.log(typeof isIsland);
-console.log(typeof country);
-console.log(typeof population);
-console.log(typeof language);
